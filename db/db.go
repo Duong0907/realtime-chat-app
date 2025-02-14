74dc8d54ab9dd97d5314082c1e9f5b11b455772f
Duong0907 <thanhduongphan12345@gmail.com>
2025-02-14 22:50:00 +0700

    add env file

--- db/db.go
+++ db/db.go
@@ -10,8 +10,10 @@ package db
 
 import (
 	"log"
+	"os"
 	"database/sql"
 	_ "github.com/lib/pq"
+	"github.com/joho/godotenv"
 )
 
 
@@ -20,7 +22,19 @@ type Database struct {
 }
 
 func NewDatabase() (*Database, error) {
-	connStr := "postgres://root:secret@localhost:5433/root?sslmode=disable"
+	// Load the .env file
+    err := godotenv.Load()
+    if err != nil {
+        log.Fatal("Error loading .env file")
+    }
+
+    // Get the connection string from the environment variable
+    connStr := os.Getenv("DB_CONN_STR")
+    if connStr == "" {
+        log.Fatal("DB_CONN_STR environment variable not set")
+    }
+
+	
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		return nil, err
